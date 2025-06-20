ac83e37490cc2c79c332b7efaf8b4678ad4d97d0
notsoexpert <notsoexpert@gmail.com>
2025-06-19 21:15:20 -0500

    Added collidable components to Explosion::Missile and Explosion::Bomb

--- src/main/entities/explosions/bomb.cpp
+++ src/main/entities/explosions/bomb.cpp
@@ -7,6 +7,7 @@ namespace Explosion::Bomb {
     static constexpr const char* Name = "Bomb";
     static constexpr OneGunGame::Images ImageID = OneGunGame::Images::ExplosionViolet;
     static constexpr sf::IntRect TextureRect = {{512, 448}, {32, 32}};
+    static constexpr sf::IntRect CollisionRect = {{0, 0}, {32, 32}};
     static constexpr size_t TotalFrames = 4U;
     static constexpr float FrameTimeInSeconds = 0.05f;
     static constexpr float LifetimeDuration = TotalFrames*FrameTimeInSeconds;
@@ -20,6 +21,7 @@ namespace Explosion::Bomb {
         setup.Registry.emplace<Component>(setup.ThisEntity, Explosion::Type::VisualOnly);
         setup.Registry.emplace<Animation>(setup.ThisEntity, renderable.Sprite.getTextureRect().position, TotalFrames, sf::seconds(FrameTimeInSeconds));
         setup.Registry.emplace<Lifetime>(setup.ThisEntity, sf::seconds(LifetimeDuration));
+        setup.Registry.emplace<Collidable>(setup.ThisEntity, CollisionRect, setup.Position, setup.Velocity, setup.Source);
     }
 
 }
