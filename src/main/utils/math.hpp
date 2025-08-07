151cc496bedf89a3341ff0105080ca34da5d2582
Jonathan McKamy <notsoexpert@gmail.com>
2025-08-07 10:55:45 -0500

    Templated GetDistanceSquared, RoundCoordinate, and RoundVector. Modified templated ConstrainValueLooped to work with any Number type. Modified LinearInterpolation to use any floating point type or floating point type Vector2. Added BezierCurve templated struct. Added templated GetQuadraticCurvePoint and GetCubicCurvePoint functions which can return any floating point type Vector2 and use any floating point type time point.

--- src/main/utils/math.hpp
+++ src/main/utils/math.hpp
@@ -2,8 +2,11 @@
 #include <concepts>
 
 namespace OneGunGame {
+
 template<typename T>
-requires std::integral<T>
+concept Number = std::integral<T> || std::floating_point<T>;
+
+template<Number T>
 T ConstrainValueLooped(T value, T min, T max) {
     if (value < min) {
         return max - (min - value) % (max - min);
@@ -13,28 +16,52 @@ T ConstrainValueLooped(T value, T min, T max) {
     return value;
 }
 
-constexpr float GetDistanceSquared(const sf::Vector2f& a, const sf::Vector2f& b){
-    return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
-}
-constexpr int RoundCoordinate(float coord) {
-    if (coord >= 0.0f) {
-        return static_cast<int>(coord + 0.5f);
-    }
-    return static_cast<int>(coord - 0.5f);
-}
-constexpr sf::Vector2i RoundVector(sf::Vector2f vec){
-    return sf::Vector2i{RoundCoordinate(vec.x), RoundCoordinate(vec.y)};
-}
-
-template<typename T>
-concept Number = std::integral<T> || std::floating_point<T>;
-
 template<Number T1, std::floating_point T2>
 constexpr T1 LinearInterpolation(T1 start, T1 end, T2 t) {
     return static_cast<T1>(start * (static_cast<T2>(1) - t) + end * t);
 }
 template<std::floating_point T2>
-constexpr sf::Vector2f LinearInterpolation(const sf::Vector2f& start, const sf::Vector2f& end, T2 t) {
+constexpr sf::Vector2<T2> LinearInterpolation(const sf::Vector2<T2>& start, const sf::Vector2<T2>& end, T2 t) {
     return start * (static_cast<T2>(1) - t) + end * t;
+}
+
+template <std::floating_point T>
+constexpr float GetDistanceSquared(const sf::Vector2<T>& a, const sf::Vector2<T>& b){
+    return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+}
+template <Number T1, std::floating_point T2>
+constexpr T1 RoundCoordinate(T2 coord) {
+    if (coord >= T2{}) {
+        return static_cast<T1>(static_cast<long long>(coord + T2{0.5f}));
+    }
+    return static_cast<T1>(static_cast<long long>(coord - T2{0.5f}));
+}
+template <std::integral T1, std::floating_point T2>
+constexpr sf::Vector2<T1> RoundVector(const sf::Vector2<T2>& vec){
+    return sf::Vector2<T1>{RoundCoordinate(vec.x), RoundCoordinate(vec.y)};
+}
+
+template <std::floating_point T, std::size_t Points>
+struct BezierCurve {
+    std::array<sf::Vector2<T>, Points> ControlPoints;
+
+    BezierCurve(std::initializer_list<sf::Vector2<t>> points) :
+        ControlPoints(points)
+    {}
+};
+
+template <std::floating_point T>
+constexpr sf::Vector2<T> GetQuadraticCurvePoint(const BezierCurve<T, 3>& curve, float timePoint) {
+    return std::pow(1.0f - timePoint, 2.0f) * curve.ControlPoints.at(0) + 
+        2.0f * (1.0f - timePoint) * timePoint * curve.ControlPoints.at(1) + 
+        std::pow(timePoint, 2.0f) * curve.ControlPoints.at(2);
+}
+template <std::floating_point T>
+constexpr sf::Vector2<T> GetCubicCurvePoint(const BezierCurve<T, 4>& curve, T timePoint) {
+    T invPoint = 1.0 - timePoint;
+    return invPoint * invPoint * invPoint * curve.ControlPoints.at(0) + 
+        T{3.0} * invPoint * invPoint * timePoint * curve.ControlPoints.at(1) + 
+        T{3.0} * invPoint * timePoint * timePoint * curve.ControlPoints.at(2) +
+        timePoint * timePoint * timePoint * curve.ControlPoints.at(3);
 }
 }
